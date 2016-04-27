e1e76c670d87df519306138122d94318c81afaee
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-04-27 00:40:08 -0700

    have parser for problem

--- stl_parser.py
+++ stl_parser.py
@@ -4,44 +4,10 @@ from funcy import cat, flatten
 
 import stl
 
-STL_GRAMMAR = Grammar(u'''
-phi = pred / finally / global / neg / or / and / "True"
-paren_phi = "(" __ phi __ ")"
-or = paren_phi _ "∨" _ paren_phi
-and = paren_phi _ "∧" _ paren_phi
-finally = "⋄" interval paren_phi
-global = "□" interval paren_phi
-neg = "¬" paren_phi
-id = "x" ~r"\d+"
-interval = "[" __ const __ "," __ const __ "]"
-pred = id _ op _ const
-const = ~r"[\+\-]?\d*(\.\d+)?"
-op = ">=" / "<=" / "<" / ">"
-_ = ~r"\s"+
-__ = ~r"\s"*
-''')
-
-class STLVisitor(NodeVisitor):
-    def generic_visit(self, _, children): return children
-    def visit_phi(self, _, children): return children[0]
-    def visit_op(self, op, _): return op.text
-    def visit_id(self, name, children): return int(name.text[1:])
-    def visit_const(self, const, children): return int(const.text)
-    def visit_paren_phi(self, _, (_1, _2, phi, _3, _4)): return phi
-    def visit_pred(self, _, (id, _1, op, _3, const)): 
-        return stl.Pred(id, op, const)
-    def visit_interval(self, _, (_1, _2, left, _3, _4, _5, right, _6, _7)): 
-        return stl.Interval(left, right)
-    def visit_finally(self, _, (_1, interval, phi)): return stl.F(phi, interval)
-    def visit_global(self, _, (_1, interval, phi)): return stl.G(phi, interval)
-    def visit_neg(self, _, (_1, phi)): return stl.Neg(phi)
-    def visit_or(self, _, (phi1, _2, _3, _4, phi2)): return stl.Or(phi1, phi2)
-    def visit_and(self, _, (phi1, _2, _3, _4, phi2)): return stl.And(phi1, phi2)
-
-STL2_GRAMMAR = Grammar(u'''
+GRAMMAR = Grammar(u'''
 psi = "--env--" EOL env* "--sys--" EOL sys*
 env = phi EOL
-sys = phi (_ rank?) EOL
+sys = phi (_ rank)? EOL
 
 rank = "[" const "]"
 
@@ -68,12 +34,13 @@ __ = ~r"\s"*
 EOL = "\\n"
 ''')
 
-class STL2Visitor(NodeVisitor):
+class Visitor(NodeVisitor):
     def generic_visit(self, _, children): return children
     def visit_phi(self, _, children): return children[0]
     def visit_env(self, _, (phi, _1)): return phi
-    def visit_sys(self, _, (phi, _1, _2)): 
-        import ipdb; ipdb.set_trace()
+    def visit_sys(self, _, (phi, maybe_rank, _2)): 
+        maybe_rank = flatten(maybe_rank)
+        rank = maybe_rank[0] if len(maybe_rank) > 0 else 0
         return phi
 
     def visit_psi(self, _, children): return children[2], children[5]
@@ -96,19 +63,12 @@ class STL2Visitor(NodeVisitor):
     def visit_or(self, _, (phi1, _2, _3, _4, phi2)): return stl.Or(phi1, phi2)
     def visit_and(self, _, (phi1, _2, _3, _4, phi2)): return stl.And(phi1, phi2)
 
-def parse_stl(stl_str):
-    return STLVisitor().visit(STL_GRAMMAR.parse(stl_str))
-
-def parse_stl2(stl_str):
-    return STL2Visitor().visit(STL2_GRAMMAR.parse(stl_str))
+def parse(stl_str):
+    return Visitor().visit(GRAMMAR.parse(stl_str))
 
 def main():
-    print(parse_stl("⋄[1,2](x1 < 2)"))
-    print(parse_stl("¬(⋄[1,2](x1 < 2))"))
-    print(parse_stl("(□[1,3](x1 < 2)) ∨ (x2 >= 4)"))
-    print(parse_stl("(⋄[1,3](□[1,3](x1 < 2))) ∧ (x2 >= 4)"))
     with open('example1.stl', 'r') as f:
-        print(parse_stl2("".join(f.readlines())))
+        print(parse("".join(f.readlines())))
 
 
 if __name__ == '__main__':
