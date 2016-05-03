57b70b3972ba419551d3b72634987e1d7f97bac5
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-05-02 21:34:43 -0700

    refactor var_map into store

--- milp.py
+++ milp.py
@@ -4,6 +4,8 @@ from math import ceil
 from itertools import product, chain, starmap
 from operator import mul
 import operator
+from collections import defaultdict
+from functools import partial
 
 from singledispatch import singledispatch
 from funcy import pairwise, mapcat
@@ -18,37 +20,54 @@ eps = 0.01 # TODO
 # - implement get_var as part of accessor
 # - automatically encode unseen variables
 
-def get_var(x, t, model, var_map):
-    if x in var_map and t in var_map[x]:
-        return var_map[x][t]
-    if x not in var_map:
-        var_map[x] = {}
+class Store(object):
+    def __init__(self, problem):
+        "docstring"
+        self.model = gpy.Model(name=problem.get('name', 'problem'))
+        self._z = defaultdict(dict)
+        self.u = defaultdict(dict)
+        self.w = defaultdict(dict)
+        self.x = defaultdict(dict)
+        self.problem = problem
 
-    vtype = gpy.GRB.BINARY if isinstance(x, stl.Pred) else gpy.GRB.CONTINUOUS
-    prefix = "z" if isinstance(x, stl.Pred) else "Z"
-    i = len(var_map)
-    name = "{}{}_{}".format(prefix, i, t)
-    var_map[x][t] = model.addVar(vtype=vtype, name=name)
-    model.update()
-    return var_map[x][t]
+        n = problem['params']['num_vars']
+        n_sys = problem['params']['num_sys_inputs']
+        n_env = problem['params']['num_env_inputs']
 
+        # Add state, input, and env vars
+        elems = [('x', n, self.x), ('u', n_sys, self.u), ('w', n_env, self.w)]
+        for pre, num, d in elems:
+            for i, t in product(range(num), range(self.steps)):
+                name ="{}{}_{}".format(pre, i, t)
+                d[i][t] = self.model.addVar(vtype=gpy.GRB.CONTINUOUS, name=name)
 
-def make_var_map(phi, model, steps, problem):
-    var_map = {}
+        self.model.update()
 
-    n = problem['params']['num_vars']
-    n_sys = problem['params']['num_sys_inputs']
-    n_env = problem['params']['num_env_inputs']
+    def z(self, x, t):
+        if x in self._z and t in self._z[x]:
+            return self._z[x][t]
 
-    # Add state, input, and env vars
-    for prefix, num in [('x', n), ('u', n_sys), ('w', n_env)]:
-        var_map[prefix] = {(i, t): model.addVar(
-            vtype=gpy.GRB.CONTINUOUS, name="{}{}_{}".format(prefix, i, t))
-                           for i, t in product(range(num), range(steps))}
+        vtype = gpy.GRB.BINARY if isinstance(x, stl.Pred) else gpy.GRB.CONTINUOUS
+        prefix = "z" if isinstance(x, stl.Pred) else "Z"
+        i = len(self._z)
+        name = "{}{}_{}".format(prefix, i, t)
+        self._z[x][t] = self.model.addVar(vtype=vtype, name=name)
+        self.model.update()
+        encode(x, t, self)
+        return self._z[x][t]
 
-    model.update()
+    @property
+    def H(self):
+        return ceil(self.problem['params']['time_horizon'])
 
-    return var_map
+    @property
+    def dt(self):
+        return self.problem['params']['dt']
+
+    @property
+    def steps(self):
+        return int(ceil(self.H / self.dt))
+
 
 @singledispatch
 def encode(problem):
@@ -57,120 +76,108 @@ def encode(problem):
     sys = reduce(stl.And, problem['sys'])
     env = reduce(stl.And, problem['env'], [])
     phi = stl.Or(stl.Neg(env), sys) if env else sys
-        
-    H = ceil(problem['params']['time_horizon'])
-    dt = problem['params']['dt']
-    steps = int(ceil(H / dt))
+    store = Store(problem)
 
-    model = gpy.Model("milp")
-    var_map = make_var_map(phi, model, steps, problem)
-    
     # encode STL constraints
-    encode(phi, model, var_map, 0, H, dt)
+    encode(phi, 0, store)
 
     # add input bounds u in [0, 1]
-    for u in chain(var_map['u'].values(), var_map['w'].values()):
-        model.addConstr(u <= 1)
-        model.addConstr(u >= 0)
+    for u in mapcat(dict.values, chain(store.u.values(), store.w.values())):
+        store.model.addConstr(u <= 1)
+        store.model.addConstr(u >= 0)
 
     # TODO: Clean up system evolution
     n = problem['params']['num_vars']
     n_sys = problem['params']['num_sys_inputs']
     n_env = problem['params']['num_env_inputs']
-    inputs = lambda t: [var_map['u'][i, t] for i in range(n_sys)] + [var_map['w'][i, t] for i in range(n_env)]
-    state = lambda t: [var_map['x'][i, t] for i in range(n)]
+    inputs = lambda t: [store.u[i][t] for i in range(n_sys)] + [store.w[i][t] for i in range(n_env)]
+    state = lambda t: [store.x[i][t] for i in range(n)]
     dot = lambda x, y: sum(starmap(mul, zip(x, y)))
     A, B = problem['state_space']['A'], problem['state_space']['B']
-    for t in range(steps-1):
+    for t in range(store.steps-1):
         for i, (A_i, B_i) in enumerate(zip(A, B)):
-            model.addConstr(var_map['x'][i, t+1] == dot(A_i, state(t)) + dot(B_i, inputs(t)))
+            store.model.addConstr(store.x[i][t+1] == dot(A_i, state(t)) + dot(B_i, inputs(t)))
 
     for psi in problem['init']:
-        x = var_map['x'][psi.lit, 0]
+        x = store.x[psi.lit][0]
         const = psi.const
-        model.addConstr(x == const)
+        store.model.addConstr(x == const)
 
     # Assert top level true
-    model.addConstr(get_var(phi, 0, model, var_map) == 1)
+    store.model.addConstr(store.z(phi, 0) == 1)
 
-    stl_vars =  mapcat(lambda x: var_map.get(x).values(), stl.walk(phi))
-    model.setObjective(sum(stl_vars), gpy.GRB.MAXIMIZE)
-    model.update()
-    model.optimize()
+    stl_vars =  mapcat(lambda phi: store.x[phi].values(), stl.walk(phi))
+    store.model.setObjective(sum(stl_vars), gpy.GRB.MAXIMIZE)
+    store.model.update()
+    store.model.optimize()
 
-    if model.status == gpy.GRB.Status.INF_OR_UNBD:
-        # Turn presolve off to determine whether model is infeasible
+    if store.model.status == gpy.GRB.Status.INF_OR_UNBD:
+        # Turn presolve off to determine whether store.model is infeasible
         # or unbounded
-        model.setParam(gpy.GRB.Param.Presolve, 0)
-        model.optimize()
+        store.model.setParam(gpy.GRB.Param.Presolve, 0)
+        store.model.optimize()
 
-    if model.status == GRB.Status.OPTIMAL:
+    if store.model.status == gpy.GRB.Status.OPTIMAL:
         # TODO: return controller
         raise NotImplementedError
-        return 
+        return
 
-    elif model.status != GRB.Status.INFEASIBLE:
+    elif store.model.status != gpy.GRB.Status.INFEASIBLE:
         # TODO: return IIS
         raise NotImplementedError
-        return 
+        return
 
 
 @encode.register(stl.Pred)
-def _(psi, model, var_map, t, H, dt):
+def _(psi, t, store):
     const = psi.const
-    x = var_map['x'][psi.lit, t]
-    z_t = get_var(psi, t, model, var_map)
+    x = store.x[psi.lit][t]
+    z_t = store.z(psi, t)
+
     if psi.op in ("<", "<=", "="):
-        model.addConstr(const - x <= M*z_t - eps)
-        model.addConstr(x - const <= M*(1 - z_t) - eps)
+        store.model.addConstr(const - x <= M*z_t - eps)
+        store.model.addConstr(x - const <= M*(1 - z_t) - eps)
 
     if psi.op in (">", ">=", "="):
-        model.addConstr(x - const <= M*z_t - eps)
-        model.addConstr(const - x <= M*(1 - z_t) - eps)
+        store.model.addConstr(x - const <= M*z_t - eps)
+        store.model.addConstr(const - x <= M*(1 - z_t) - eps)
 
 
 @encode.register(stl.Or)
-def _(psi, model, var_map, t, H, dt):
-    encode_bool_op(psi, model, var_map, t, H, dt, False)
+def _(psi, t, store):
+    encode_bool_op(psi, t, store, False)
 
 
 @encode.register(stl.And)
-def _(psi, model, var_map, t, H, dt):
-    encode_bool_op(psi, model, var_map, t, H, dt, True)
+def _(psi, t, store):
+    encode_bool_op(psi, t, store, True)
 
-    
-def encode_bool_op(psi, model, var_map, t, H, dt, or_flag):
-    z_psi = get_var(psi, t, model, var_map)
-    elems = [get_var(psi.left, t, model, var_map), 
-             get_var(psi.right, t, model, var_map)]
 
+def encode_bool_op(psi, t, store, or_flag):
+    z_psi = store.z(psi, t)
+    elems = [store.z(psi.left, t), store.z(psi.right, t)]
     encode_op(z_psi, elems, model, or_flag=or_flag)
-
-    encode(psi.left, model, var_map, t, H, dt)
-    encode(psi.right, model, var_map, t, H, dt)
 
 
 @encode.register(stl.F)
-def _(psi, model, var_map, t, H, dt):
-    encode_temp_op(psi, model, var_map, t, H, dt, or_flag=True)
+def _(psi, t, store):
+    encode_temp_op(psi, t, store, or_flag=True)
 
 
 @encode.register(stl.G)
-def _(psi, model, var_map, t, H, dt):
-    encode_temp_op(psi, model, var_map, t, H, dt, or_flag=False)
+def _(psi, t, store):
+    encode_temp_op(psi, t, store, or_flag=False)
 
 
-def encode_temp_op(psi, model, var_map, t, H, dt, or_flag=False):
-    z_psi = get_var(psi, t, model, var_map)
+def encode_temp_op(psi, t, store, or_flag=False):
+    z_psi = store.z(psi, t)
     a, b = psi.interval.lower, psi.interval.upper
-    f = lambda x: int(ceil(x / dt))
+    f = lambda x: int(ceil(x / store.dt))
+    H = store.H
     a, b = f(min(t + a, H)), f(min(t + b, H))
-    
-    elems = [get_var(psi.arg, i, model, var_map) for i in range(a, b + 1)]
-    encode_op(z_psi, elems, model, or_flag=or_flag)
 
-    for i in range(a, b + 1):
-        encode(psi.arg, model, var_map, t+i, H, dt)    
+    elems = [store.z(psi.arg, t + i) for i in range(a, b + 1)]
+    encode_op(z_psi, elems, store.model, or_flag=or_flag)
 
 
 def encode_op(z_psi, elems, model, or_flag=False):
@@ -188,20 +195,22 @@ def encode_op(z_psi, elems, model, or_flag=False):
     model.addConstr(rel(lhs, z_psi))
 
 
+@encode.register(stl.Neg)
+def _(psi, t, store):
+    z_psi = store.z(psi, t)
+    z_phi = store.z(psi.arg, t)
+    model.addConstr(z_psi == 1 - z_phi)
+
+    encode(psi.arg, t, store)
+
+
+# TODO
 @encode.register(stl.FG)
 def _(psi, model, var_map, t, H, dt):
     raise NotImplementedError
 
 
+# TODO
 @encode.register(stl.GF)
 def _(psi, model, var_map, t, H, dt):
     raise NotImplementedError
-
-
-@encode.register(stl.Neg)
-def _(psi, model, var_map, t, H, dt):
-    z_psi = get_var(psi, t, model, var_map)
-    z_phi = get_var(psi.arg, t, model, var_map)
-    model.addConstr(z_psi == 1 - z_phi)
-
-    encode(psi.arg, model, var_map, t, H, dt)
