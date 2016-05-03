9fe82f9f379b2b599be001064fcbbbafeabadd48
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-05-03 15:24:02 -0700

    add more semantic constraint names + tracking

---
+++ constraint_kinds.py
@@ -0,0 +1,22 @@
+from enum import Enum, unique
+
+@unique
+class Kind(Enum):
+    INIT = 1
+    DYNAMICS = 2
+    SYS_INPUT_UPPER = 3
+    SYS_INPUT_LOWER = 4
+    ENV_INPUT_UPPER = 5
+    ENV_INPUT_LOWER = 6
+    ASSERT_FEASIBLE = 7
+    PRED_UPPER = 8
+    PRED_LOWER = 9
+    NEG = 10
+    F = 11
+    F_TOTAL = 12
+    G = 13
+    G_TOTAL = 14
+    OR = 15
+    OR_TOTAL = 16
+    AND = 17
+    AND_TOTAL = 18
--- milp.py
+++ milp.py
@@ -9,17 +9,17 @@ from __future__ import division
 from math import ceil
 from itertools import product, chain, starmap
 import operator
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 from singledispatch import singledispatch
 from funcy import mapcat, pluck, group_by
 import gurobipy as gpy
 
 import stl
+from constraint_kinds import Kind as K
 
 M = 10000  # TODO
 eps = 0.01  # TODO
-
 
 class Store(object):
     def __init__(self, problem):
@@ -29,7 +29,8 @@ class Store(object):
         self.u = defaultdict(dict)
         self.w = defaultdict(dict)
         self.x = defaultdict(dict)
-        self.constr_lookup = dict
+        self.constr_lookup = defaultdict(dict)
+        self._constr_counter = Counter()
         self.problem = problem
 
         n = problem['params']['num_vars']
@@ -73,8 +74,11 @@ class Store(object):
         return int(ceil(self.H / self.dt))
 
     def add_constr(self, constr, phi=None, kind=None):
-        r = self.model.addConstr(constr)
-        self.constr_lookup[r] = (phi, kind)
+        self._constr_counter.update([kind])
+        name = "{}{}".format(kind.name, self._constr_counter[kind])
+        r = self.model.addConstr(constr, name=name)
+        self.model.update()
+        self.constr_lookup[r.ConstrName] = (phi, kind)
 
 
 def encode_state_evolution(store, problem):
@@ -85,7 +89,16 @@ def encode_state_evolution(store, problem):
     for t in range(store.steps - 1):
         for i, (A_i, B_i) in enumerate(zip(A, B)):
             constr = store.x[i][t + 1] == dot(A_i, state(t)) + store.dt*dot(B_i, inputs(t))
-            store.add_constr(constr, kind="Dynamics")
+            store.add_constr(constr, kind=K.DYNAMICS)
+
+def encode_input_constr(store, env=False):
+    # add input bounds u in [0, 1]
+    inputs = store.w if env else store.u
+    k1 = K.ENV_INPUT_UPPER if env else K.SYS_INPUT_UPPER
+    k2 = K.ENV_INPUT_LOWER if env else K.SYS_INPUT_LOWER
+    for u in mapcat(dict.values, inputs.values()):
+        store.add_constr(u <= 1, kind=k1)
+        store.add_constr(u >= 0, kind=k2)
 
 
 @singledispatch
@@ -100,28 +113,27 @@ def encode(problem):
     # encode STL constraints
     encode(phi, 0, store)
 
-    # add input bounds u in [0, 1]
-    for u in mapcat(dict.values, chain(store.u.values(), store.w.values())):
-        store.add_constr(u <= 1, kind="input_upper")
-        store.add_constr(u >= 0, kind="input_lower")
+    encode_input_constr(store)
+    encode_input_constr(store, env=True)
 
     encode_state_evolution(store, problem)
 
     for psi in problem['init']:
         x = store.x[psi.lit][0]
         const = psi.const
-        store.add_constr(x == const, kind="initial")
+        store.add_constr(x == const, kind=K.INIT)
 
     # Assert top level true
-    store.add_constr(store.z(phi, 0) == 1, kind="Check_Feasible")
+    store.add_constr(store.z(phi, 0) == 1, kind=K.ASSERT_FEASIBLE)
 
     # Create Objective
-    stl_vars = mapcat(lambda phi: store.x[phi].values(), stl.walk(phi))
+    stl_vars = mapcat(dict.values, store._z.values())
     # TODO: support alternative objective functions
+    
     store.model.setObjective(sum(stl_vars), gpy.GRB.MAXIMIZE)
 
     store.model.update()
-    return store.model
+    return store.model, store
 
 
 @encode.register(stl.Pred)
@@ -130,42 +142,43 @@ def _(psi, t, store):
     x = store.x[psi.lit][t]
     z_t = store.z(psi, t)
 
+    # TODO combine
     if psi.op in ("<", "<=", "="):
-        store.add_constr(const - x <= M * z_t - eps, kind="")
-        store.add_constr(x - const <= M * (1 - z_t) - eps, kind="")
+        store.add_constr(const - x <= M * z_t - eps, phi=psi, kind=K.PRED_UPPER)
+        store.add_constr(x - const <= M * (1 - z_t) - eps, phi=psi, kind=K.PRED_LOWER)
 
     if psi.op in (">", ">=", "="):
-        store.add_constr(x - const <= M * z_t - eps)
-        store.add_constr(const - x <= M * (1 - z_t) - eps)
+        store.add_constr(x - const <= M * z_t - eps, phi=psi, kind=K.PRED_UPPER)
+        store.add_constr(const - x <= M * (1 - z_t) - eps, phi=psi, kind=K.PRED_LOWER)
 
 
 @encode.register(stl.Or)
 def _(psi, t, store):
-    encode_bool_op(psi, t, store, False)
+    encode_bool_op(psi, t, store, (K.OR, K.OR_TOTAL), False)
 
 
 @encode.register(stl.And)
 def _(psi, t, store):
-    encode_bool_op(psi, t, store, True)
+    encode_bool_op(psi, t, store, (K.AND, K.AND_TOTAL), True)
 
 
-def encode_bool_op(psi, t, store, or_flag):
+def encode_bool_op(psi, t, store, kind, or_flag):
     z_psi = store.z(psi, t)
     elems = [store.z(psi.left, t), store.z(psi.right, t)]
-    encode_op(z_psi, elems, model, or_flag=or_flag)
+    encode_op(z_psi, elems, model, psi, kind, or_flag=or_flag)
 
 
 @encode.register(stl.F)
 def _(psi, t, store):
-    encode_temp_op(psi, t, store, or_flag=True)
+    encode_temp_op(psi, t, store, (K.F, K.F_TOTAL), or_flag=True)
 
 
 @encode.register(stl.G)
 def _(psi, t, store):
-    encode_temp_op(psi, t, store, or_flag=False)
+    encode_temp_op(psi, t, store, (K.G, K.G_TOTAL), or_flag=False)
 
 
-def encode_temp_op(psi, t, store, or_flag=False):
+def encode_temp_op(psi, t, store, kind, or_flag=False):
     z_psi = store.z(psi, t)
     a, b = psi.interval.lower, psi.interval.upper
     f = lambda x: int(ceil(x / store.dt))
@@ -173,10 +186,10 @@ def encode_temp_op(psi, t, store, or_flag=False):
     a, b = f(min(t + a, H)), f(min(t + b, H))
 
     elems = [store.z(psi.arg, t + i) for i in range(a, b + 1)]
-    encode_op(z_psi, elems, store, or_flag=or_flag)
+    encode_op(z_psi, elems, store, kind, psi, or_flag=or_flag)
 
 
-def encode_op(z_psi, elems, store, or_flag=False):
+def encode_op(z_psi, elems, store, (k1, k2), phi, or_flag=False):
     z_phi_total = sum(elems)
 
     if or_flag:
@@ -187,18 +200,18 @@ def encode_op(z_psi, elems, store, or_flag=False):
         lhs = 1 - len(elems) + z_phi_total
 
     for e in elems:
-        store.add_constr(rel(z_psi, e))
-    store.add_constr(rel(lhs, z_psi))
+        store.add_constr(rel(z_psi, e), phi, kind=k1)
+    store.add_constr(rel(lhs, z_psi), phi, kind=k2)
 
 
 @encode.register(stl.Neg)
 def _(psi, t, store):
     z_psi, z_phi = store.z(psi, t), store.z(psi.arg, t)
-    store.add_constr(z_psi == 1 - z_phi, psi)
+    store.add_constr(z_psi == 1 - z_phi, psi, kind=K.Neg)
 
 
 def encode_and_run(problem):
-    model = encode(problem)
+    model, store = encode(problem)
     model.optimize()
 
     if model.status == gpy.GRB.Status.INF_OR_UNBD:
@@ -209,7 +222,7 @@ def encode_and_run(problem):
 
     if model.status == gpy.GRB.Status.INFEASIBLE:
         model.computeIIS()
-        IIS = [x.ConstrName for x in model.getConstrs() if x.IISConstr]
+        IIS = [store.constr_lookup[x.ConstrName] for x in model.getConstrs() if x.IISConstr]
         return (False, IIS)
 
     elif model.status == gpy.GRB.Status.OPTIMAL:
