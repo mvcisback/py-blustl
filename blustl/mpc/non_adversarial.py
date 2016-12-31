3217168daf44e840d3d8a8c58ef3017028a1b32a
Marcell Vazquez-Chanlatte <mvc@linux.com>
2016-12-30 22:31:29 -0800

    implement MPC as coroutine

--- blustl/mpc/non_adversarial.py
+++ blustl/mpc/non_adversarial.py
@@ -2,17 +2,22 @@ from collections import deque
 
 from blustl.game import mpc_games_sl_generator, Game
 
-def measurements():
-    #TODO
-    pass
-
-
-def queue_to_stl():
-    pass
+def queue_to_stl(g:Game, q):
+    """Takes measurements and writes appropriate STL.
+    Currently assumes piecewise interpolation of measurements.
+    TODO: Incorporate Lipshitz bound to bound measurements
+    """
+    return [stl.G(stl.Interval(t, t+g.model.dt)) for t, phi in enumerate(q)]
 
 
 def mpc_game(g:Game):
+    """Co-routine:
+      - Yields: MPC STL
+      - Recieves: Measurement STL
+
+    TODO: Incorporate Lipshitz bound to bound measurements
+    """
     q = deque([], mexlen=g.model.N)
-    for m, phi in zip(measurements, mpc_games_sl_generator(g)):
-        q.append(m)
-        yield stl.And((phi, queue_to_stl(q)))
+    for phi in mpc_games_sl_generator(g):
+        measurements = yield stl.And(tuple([phi] + queue_to_stl(g, q)))
+        q.append(measurement)
