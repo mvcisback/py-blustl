0de00b1bdc28095e38b04711359b2e0e13837fc6
Marcell Vazquez-Chanlatte <mvc@linux.com>
2017-01-08 15:05:48 -0800

    cegis only for system

--- blustl/mpc/adversarial.py
+++ blustl/mpc/adversarial.py
@@ -6,21 +6,16 @@ import funcy as fn
 from blustl.mpc.non_adversarial import predict
 from blustl.utils import to_lineq
 
-def cegis(phi, g, t, *, p1:bool):
-    banned_ws, banned_us = set(), set()
-    turns = [(True, banned_us, phi, g.model.vars.inputs),
-             (False, banned_ws, ~phi, g.model.vars.env)]
-    for p1_turn, banned_inputs, banned_adv_inputs, spec, input_symbols in turns:
-        psi = stl.andf(*banned_inputs)
+def cegis(phi, g, t):
+    banned_us = set()
+    input_symbols = g.model.vars.inputs
+    while True:
+        psi = ~stl.orf(*banned_us)
         success, inputs = best_response(psi, g, t, input_symbols)
-        
-        if success:
-            banned_adv_inputs.add(inputs)
-        else:
-            return p1_turn, inputs, banned_adv_inputs
+        return  inputs, banned_adv_inputs
 
 
 def best_response(psi, g, t, input_symbols):
     res = predict(psi, g, t)
     inputs = fn.project(res.solution.get(t, {}), input_symbols)
-    return res.feasible, ~to_lineq(inputs)
+    return res.feasible, to_lineq(inputs)
