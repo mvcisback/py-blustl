10adb40d2c2265318346f661bdfaf7e4d41f3036
Marcell Vazquez-Chanlatte <mvc@linux.com>
2017-12-17 18:17:20 -0800

    generalized RPS is encoded

--- magnum/examples/rock_paper_scissors_generator.py
+++ magnum/examples/rock_paper_scissors_generator.py
@@ -37,29 +37,70 @@ CONTEXT = {
     parse("(PaperBeatsRock) & (ScissorsBeatsPaper) & (RockBeatsScissors)"),
 }
 
-def create_paper_scissors(f1, f2, k=0, p1=True, delta=10):
+
+def create_intervals(f1, f2, k=0, p1=True, delta=10):
     name = 'x' if p1 else 'y'
     phi = stl.parse(f'({name} >= a?) & ({name} < b?)')
-    
+
     def intvl_to_spec(itvl):
         return phi.set_params({'a?': itvl[0], 'b?': itvl[1]})
 
-    intervals = ((f1(i), f2(i)) for i in range(k+1))
-    
-    return stl.andf(*map(intvl_to_spec, intervals))
+    intervals = ((f1(i), f2(i)) for i in range(k + 1))
+
+    return stl.orf(*map(intvl_to_spec, intervals))
 
 
-def create_paper(k=0, p1=True, delta=10):
-    return create_paper_scissors(k=k, p1=p1, delta=delta, 
-                                 f1=lambda i: delta*(1 + 6*i),
-                                 f2=lambda i: delta*(3 + 6*i))
+def create_paper(k=0, p1=True, delta=10, eps=0):
+    return create_intervals(
+        k=k,
+        p1=p1,
+        delta=delta,
+        f1=lambda i: delta * (1 + 6 * i),
+        f2=lambda i: delta * (3 + 6 * i))
 
 
-def create_rps_game(n=1, spock=0):
+def create_scissors(k=0, p1=True, delta=10, eps=0):
+    return create_intervals(
+        k=k,
+        p1=p1,
+        delta=delta,
+        f1=lambda i: delta * (3 + 6 * i),
+        f2=lambda i: delta * (5 + 6 * i))
+
+
+def create_rock(k=0, p1=True, delta=10, eps=0):
+    itvls = create_intervals(
+        k=k + 1,
+        p1=p1,
+        delta=delta,
+        f1=lambda i: delta * (-1 + 6 * i),
+        f2=lambda i: delta * (1 + 6 * i))
+
+    # Hack: to match other rps encoding, we remove first and last one.
+    name = 'x' if p1 else 'y'
+    start = stl.parse(f'{name} < {delta}')
+    end = stl.parse(f'{name} >= {delta*((k+1)*6 - 1)}')
+    middle = stl.BOT if k == 0 else stl.orf(*itvls.args[1:-1])
+    return start | middle | end
+
+
+def create_rps_game(n=1, eps=0):
     assert n >= 1
-    
+
+    k = n - 1
+    delta = 60 / (6 * n)
     # TODO
-    context = CONTEXT
+    context = {
+        'xRock': create_rock(k=k, p1=True, delta=delta, eps=eps),
+        'xScissors': create_scissors(k=k, p1=True, delta=delta, eps=eps),
+        'xPaper': create_paper(k=k, p1=True, delta=delta, eps=eps),
+        'yRock': create_rock(k=k, p1=False, delta=delta),
+        'yScissors': create_scissors(k=k, p1=False, delta=delta),
+        'yPaper': create_paper(k=k, p1=False, delta=delta),
+    }
+
+    context = fn.walk_keys(stl.parse, context)
+    context.update(CONTEXT)
 
     spec = G.Specs(
         obj=stl.parse('G(Rules)', H=H).inline_context(context),
@@ -67,4 +108,4 @@ def create_rps_game(n=1, spock=0):
         init=stl.parse("Init").inline_context(context),
     )
 
-    rps = G.Game(specs=spec, model=model)
+    return G.Game(specs=spec, model=MODEL)
--- magnum/examples/test_rps_generator.py
+++ magnum/examples/test_rps_generator.py
@@ -2,15 +2,52 @@ import stl
 
 from magnum.examples import rock_paper_scissors_generator as rpsg
 
+
 def test_create_paper():
     psi = rpsg.create_paper(k=0)
     phi0 = stl.parse('(x >= 10) & (x < 30)')
-    
+
     assert phi0 == psi
 
     psi = rpsg.create_paper(k=2, p1=False)
     phi0 = stl.parse('(y >= 10) & (y < 30)')
     phi1 = stl.parse('(y >= 70) & (y < 90)')
     phi2 = stl.parse('(y >= 130) & (y < 150)')
-    
-    assert phi0 & phi1 & phi2 == psi
+
+    assert phi0 | phi1 | phi2 == psi
+
+
+def test_create_scissors():
+    psi = rpsg.create_scissors(k=0)
+    phi0 = stl.parse('(x >= 30) & (x < 50)')
+
+    assert phi0 == psi
+
+    psi = rpsg.create_scissors(k=2, p1=False)
+    phi0 = stl.parse('(y >= 30) & (y < 50)')
+    phi1 = stl.parse('(y >= 90) & (y < 110)')
+    phi2 = stl.parse('(y >= 150) & (y < 170)')
+
+    assert phi0 | phi1 | phi2 == psi
+
+
+def test_create_rock():
+    psi = rpsg.create_rock(k=0)
+    phi0 = stl.parse('(x < 10)')
+    phi1 = stl.parse('(x >= 50)')
+
+    assert phi0 | phi1 == psi
+
+    psi = rpsg.create_rock(k=2, p1=False)
+    phi0 = stl.parse('(y < 10)')
+    phi1 = stl.parse('(y >= 50) & (y < 70)')
+    phi2 = stl.parse('(y >= 110) & (y < 130)')
+    phi3 = stl.parse('(y >= 170)')
+
+    assert phi0 | phi1 | phi2 | phi3 == psi
+
+
+def test_create_rps_game():
+    from magnum.examples.rock_paper_scissors import rps as g
+    g2 = rpsg.create_rps_game(n=1, eps=0)
+    assert g.specs == g2.specs
